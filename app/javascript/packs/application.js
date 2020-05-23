5662ecda996b829461a2fbe1c916cb8757ae0fbd
Kaloyan Atansov <kaloyanatanasov83@gmail.com>
2020-05-23 01:46:27 +0100

    Add styling to the index page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,7 +14,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
 
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
@@ -28,22 +26,28 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
+document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
   $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
   });
 });
 
+$("#validate_image").bind("change", function () {
+  const size_in_megabytes = this.files[0].size / 1024 / 1024;
+  if (size_in_megabytes > 5) {
+    alert("Maximum file size is 5MB. Please choose a smaller file.");
+    $("#validate_image").val("");
+  }
+});
 
-
-  $("#validate_image").bind("change", function() {
-    const size_in_megabytes = this.files[0].size/1024/1024;
-    if (size_in_megabytes > 5) {
-      alert("Maximum file size is 5MB. Please choose a smaller file.");
-      $("#validate_image").val("");
-    }
-  });
-
-
+$(document).on("input", "textarea", function () {
+  $(this).outerHeight(38).outerHeight(this.scrollHeight); // 38 or '1em' -min-height
+});
+// $('textarea').each(function () {
+//   this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+// }).on('input', function () {
+//   this.style.height = 'auto';
+//   this.style.height = (this.scrollHeight) + 'px';
+// });
